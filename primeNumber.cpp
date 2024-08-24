b66ed0f6db89d883cb28ab4f68b1d557a908f9ca
Rameen Ejaz <rameen@Rameens-MacBook-Air.local>
2024-08-24 15:39:32 +0500

    how to locate a prime number

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout<<"Hello Rameen"<<'\n';
-    
-}
+int main () {
+    int number;
+    bool isPrime = true;
+
+    cout<< "enter a number n"<<'\n';
+    cin>>number;
+
+    for (int i = 2; i<number; i++) {
+        if (number%i==0) {
+            isPrime=false;
+            break;
+        }
+    }
+
+    if (isPrime==false) {
+       cout<<"not prime" <<'\n';
+    } else {
+        
+    cout<<"prime" <<'\n';
+    }
+
+
+    return 0;
+    }
