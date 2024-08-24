71f87d4de48372aaea5d376cfeec929c74c1f21b
Rameen Ejaz <rameen@Rameens-MacBook-Air.local>
2024-08-24 13:19:35 +0500

    repository clean-up

--- haris.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World! Haris" << endl;
-    cout << "Haris" << endl; 
-    cout << "Rameen" << endl; 
-    return 0; 
-}
