e1b5bad4844cfcd6721ce12e9037b1ada7a97dab
Rameen Ejaz <rameen@Rameens-MacBook-Air.local>
2024-08-25 20:44:08 +0500

    multiplication table

---
+++ multiplication-table.cpp
@@ -0,0 +1,19 @@
+// Question: Write a program that prints the multiplication table for a number provided by the user. Use a for loop.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int product = 0;
+    int number; // write the number's table
+    int output;
+
+    cout << "enter a number n: ";
+    cin >> number;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        product = number * i;
+        cout << number << "*" << i << " = "<< product <<'\n';
+    }
+}
