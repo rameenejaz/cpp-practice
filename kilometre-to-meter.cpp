b4abf909f610c98ad34e993fe17bcde52e682874
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-01 17:15:02 +0500

    how to convert from kilometer to meter

---
+++ kilometre-to-meter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    float kilometer;
+    float metre;
+
+    cout<<"Enter the value in Kilometer:";
+    cin>>kilometer;
+
+    metre = (kilometer * 1000);
+    cout<<"value in metre:"<<(kilometer*1000)<<'\n';
+
+
+}
