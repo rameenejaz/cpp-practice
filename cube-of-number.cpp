ccf5c861784fe976998cb91dfd468cf8cb350ccf
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-01 13:45:11 +0500

    how to find cube of number

---
+++ cube-of-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int number;
+    cout<<"Enter a number:";
+    cin>>number;
+
+    int cube;
+    cube= (number*number*number);
+    cout<<"cube of the number"<<cube<<'\n';
+
+}
