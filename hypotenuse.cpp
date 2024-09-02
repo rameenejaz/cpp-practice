da0600eff72de1610f1992a9acb2ebf17fd2bcf9
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-02 21:56:33 +0500

    finding hypotenuse

---
+++ hypotenuse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float base = 0;
+    float height = 0;
+    float hypotenuse = 0;
+
+    cout << "Enter the value of base:";
+    cin >> base;
+    cout << "Enter the value of height:";
+    cin >> height;
+
+    hypotenuse = sqrt((base * base) + (height * height));
+    cout << "hypotenuse= " << hypotenuse<<'\n';
+}
