22df45d78fd0410de325230dd184d4d2982afa2c
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-03 23:42:01 +0500

    working on adoption of animal

---
+++ person-deatils.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main () {
+    string name;
+    string city;
+    string nameOfCollege;
+    string proffesion;
+    string typeOfAnimal;
+    string petName;
+    int age=0;
+    
+    cout<<"Enter name:";
+    cin>>name;
+    cout<<"Enter city name:";
+    cin>>city;
+    cout<<"Enter nameOfCollege:";
+    cin>>nameOfCollege;
+    cout<<"Enter proffesion:";
+    cin>>proffesion;
+    cout<<"Enter the type of animal:";
+    cin>>typeOfAnimal;
+    cout<<"Enter pet's name:";
+    cin>>petName;
+    cout<<"Enter age:";
+    cin>>age;
+
+    cout<<"There once was a person named "<< name << "who lived in  . " <<city <<""."<< "At the age of  ,  <<age,name<< "went to college at  "<<nameOfCollege << "."<<name<<"graduated and went to work as a "<<proffesion<<"."<<"Then,"<<name<<"adopted a(n) "<<typeOfAnimal<<"named"<<petName<<"."<<"They both lived happily ever after!"
+}
