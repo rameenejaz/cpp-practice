7334550c59bb4b6d355341cee39f2de5b8d9b79e
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-07 20:58:02 +0500

    printing numbers from 1 to n

---
+++ print-from-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int number;
+    int i;
+    cout<<"enter a number:"<<'\n';
+    cin>>number;
+
+    for (i=1;i<=number;i++) {
+        cout<<i<<'\n';
+    }
+
+}
