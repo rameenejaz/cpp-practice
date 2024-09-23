09e5c491437e4fecf22878883480cad40844d6f3
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-23 23:03:12 +0500

    Finding sum of the natural numbers

---
+++ functions-9.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int factorial (int number) {
+    answer= number!;
+    
+}
---
+++ while-loop-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int number=0;
+    int sum=0;
+    int i=0;
+
+    cout<<"Enter a positive number: ";
+    cin>>number;
+
+    while (number>=i) {
+        sum=sum+i;
+        i++;
+
+    }
+    cout<<"The sum of the numbers is: "<<sum;
+}
