fc78fdecc938763291114785360c5e888b756d20
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-24 23:36:09 +0500

    multiplying two numbers

---
+++ while-loop-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int number=1;
+    int i=1;
+    int product=1;
+    cout<<"Enter the number to display its multiplication table: ";
+    cin>>number;
+    cout<<'\n';
+
+    while (i>=10) {
+        product=number*i;
+    }
+    cout<<number<<"*"<<i<<"="<<product;
+        i++;
+
+}
