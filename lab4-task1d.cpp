818daea861b5dfc95c6539e749099b4c31d50ef4
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-28 23:53:36 +0500

    using ternary operators

---
+++ lab4-task1d.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int y=0;
+    int x=0;
+    int z=0;
+    cout<<"(x >= z || (x + y >= z): "<< ((x>=z) || (x + y >= z)? "True": "False")<<'\n';
+
+}
