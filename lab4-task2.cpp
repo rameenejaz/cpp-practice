7cd7603df11a816c8d47f7830952bc048b0ee7a7
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-29 13:18:03 +0500

    using a new formula with if statements

---
+++ lab4-task2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+int x=3;
+int y=4;
+int z=7;
+int w=1;
+
+cout << "x == y: " << (x == y) << endl;
+cout << "x != z: " << (x != z ) << endl;
+cout << "y == z - 3: "<< (y == (z-3)) << endl;
+cout << "!(z > w): " << !(z > w) << endl;
+cout << "x + y < z: " <<((x + y)<z ) << endl;
+}
---
+++ lab4-task3a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+bool b1=true; 
+bool b2=false; 
+bool b3=(b1==b2);
+if (b3==1) {
+    cout<<"True"<<'\n';
+}
+else {
+    cout<<"False"<<'\n';
+}
+}
