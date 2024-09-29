4072c7640a6bea8e83d2de93c7ad0c5cc63811b0
Rameen Ejaz <rameenejaz4@gmail.com>
2024-09-29 13:34:26 +0500

    using if conditions

---
+++ lab4-task-4a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int X=0;
+cout<<"Enter the value of X (1, -1 or 0)";
+cin>>X;
+if (X>0) {
+    cout<<"1";
+}
+else if (X<0) {
+    cout<<"-1";
+}
+else if (X==0) {
+    cout<<"0";
+}
+}
