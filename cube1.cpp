02d2c5a41f9fda0f023a66d3908cb04b8ef98f1a
Rameen Ejaz <rameenejaz4@gmail.com>
2024-10-01 20:33:34 +0500

    finding cube of a number

---
+++ cube1.cpp
@@ -0,0 +1,11 @@
+//finding the cube of a number
+#include <iostream>
+using namespace std;
+int main () {
+    int cubeOfNumber=0;
+    int number;
+    cout<<"Enter the number to cube:";
+    cin>>number;
+    cubeOfNumber=number*number*number;
+    cout<<"The cube of "<<number<<" is "<<cubeOfNumber;
+}
