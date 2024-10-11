60328414e92891342e3643c3b95bf662494fed60
Rameen Ejaz <rameenejaz4@gmail.com>
2024-10-11 19:46:46 +0500

    finding average of 3 numbers using functions

--- avg-func.cpp
+++ avg-func.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
-void greet() {
-    cout<<"I am learning functions in C++!"<<'\n';
+float averageNum(int a, int b, int c) {
+    float average=(a+b+c)/3;
+    return average;
 }
 int main () {
-    greet();
+    int a;
+    int b;
+    int c;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"Enter the value of c: ";
+    cin>>c;
+    int result=averageNum(a,b,c);
+    cout<<"The average is "<<result;
 }
-
