71091006221f43bf0c95768cc74d83ac539f0496
Rameen Ejaz <rameenejaz4@gmail.com>
2024-11-06 23:31:15 +0500

    printing patterns with functions

---
+++ lab7-task2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int pattern(int num) {
+    for (int i=0; i<num; i++) {
+        for (int j=0; j<num; j++) {
+            cout<<"*";
+        }
+        cout<<'\n';
+    }
+}
+int main () {
+    int sides;
+    cout<<"Enter a number to print parameter sides: ";
+    cin>>sides;
+    cout<<"This is the pattern: "<<'\n';
+    pattern(sides);
+}
