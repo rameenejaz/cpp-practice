2a97ef5cf4eedb4f00980bb13ea886757ea774fd
Rameen Ejaz <rameenejaz4@gmail.com>
2024-12-03 23:07:18 +0500

    learning how strings work

---
+++ strings4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main () {
+string inputString;
+cout<<"Enter a word: ";
+cin>>inputString;
+int length=inputString.length();
+cout<<"The length is "<<length;
+}
