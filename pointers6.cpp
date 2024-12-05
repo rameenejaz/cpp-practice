c6cdd2c708fa40830a9b2f81dfd087d5944aa243
Rameen Ejaz <rameenejaz4@gmail.com>
2024-12-05 20:26:54 +0500

    swapping two numbers using pointers

---
+++ pointers6.cpp
@@ -0,0 +1,13 @@
+//swap two numbers using pointers
+#include <iostream>
+using namespace std;
+void swap(int *a, int *b) {
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main () {
+    int x=10, y=20;
+    swap(&x,&y);
+    cout<<x<<" "<<y<<'\n';
+}
