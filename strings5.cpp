9a81d1211e6583438f678760ca880c0365b8a27f
Rameen Ejaz <rameenejaz4@gmail.com>
2024-12-08 02:12:03 +0500

    learning how to work with structs

---
+++ strings5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    string str1="rameen,ejaz";
+    string str2="ejaz";
+    int pos=str1.find(str2);
+    if ()
+
+
+}
---
+++ structs1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+struct Student {
+        int id;
+        string name;
+        float grade;
+    };
+int main () {
+    Student student1;
+    student1.id=101;
+    student1.name="rameen";
+    student1.grade=55.5;
+    cout<<"Student information: "<<'\n';
+    cout<<"ID: "<<student1.id<<'\n';
+    cout<<"Name: "<<student1.name<<'\n';
+    cout<<"Grade: "<<student1.grade<<'\n';
+    }
