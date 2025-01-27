61b50880280a317d751cd1fb80171fbe9fed0110
Rameen Ejaz <rameenejaz4@gmail.com>
2025-01-28 01:31:30 +0500

    learning how the copy constructor works

---
+++ copyConstructor.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+class Hero {
+    private:
+    int health;
+    public:
+    string name;
+    //invoke the default constructor:
+    Hero () {
+        cout<<"Constructor Made!"<<'\n';
+    }
+    void print () {
+        cout<<"health:"<<health<<'\n';
+        cout<<"name:"<<name<<'\n';
+    }
+    //to get the health
+    int getHealth() {
+        return health;
+    }
+    string getName() {
+        return name;
+    }
+    //to set health
+    void setHealth(int h) {
+        health=h;
+    }
+    void setName(string newName) {
+        name=newName;
+    }
+    Hero (int health) {
+        this ->health=health;
+    }
+    Hero (int health, string name) {
+        this ->health=health;
+        this ->name=name;
+    }
+};
+int main () {
+    Hero S(10,"donald");
+    S.print();
+    Hero R(S);
+    R.print();
+}
