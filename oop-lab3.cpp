ed6dc85b6a43a5cd6423dc1eb6d48e70f1619914
Rameen Ejaz <rameenejaz4@gmail.com>
2025-02-25 23:33:39 +0500

    use of get and set functions

--- oop-lab3.cpp
+++ oop-lab3.cpp
@@ -1,105 +1,101 @@
 #include <iostream>
 using namespace std;
-class studentData{
-    private:
-    string name;
-    int age;
-    public:
-    void enterInfo(string &inputName, int &inputAge) {
-        name=inputName;
-        age=inputAge;
-        cout<<"DATA HAS BEEN ENTERED!"<<'\n';
-    }
-    void editInfo(string &inputName, int &inputAge ) {
-        name=inputName;
-        age=inputAge;
-        // char choice;
-        // char choice2;
-        // do {
-        //     cout<<"Do you want to edit student information? (Y/N):";
-        //     cin>>choice;
-        //     cout<<'\n';
-        //     if (choice=='Y' || choice=='y') {
-        //         cout<<"Do you want to edit name or age?(N/A):";
-        //         cin>>choice2;
-        //         cout<<'\n';
-        //         if (choice2=='N'|| choice2=='n'){
-        //             break;
-        //         }
-        //         if (choice2=='A'|| choice=='a'){
-        //             cout<<"Enter age again:";
-        //             cin>>inputAge;
-        //             age=inputAge;
-        //             cout<< age;
-        //             cout<<'\n';
-        //         }
-        //     }
-        // }while (choice=='Y' || choice=='y');
-        // cout<<"DATA HAS BEEN EDITED"<<'\n';
-    }
-    void deleteData() {
-        name=nullptr;
-        age=0;
-        cout<<"DATA HAS BEEN DELETED!"<<'\n';
-    }
-    // void viewData() {
-    //     cout<<"Name:"<<name;
-    //     cout<<"Age:"<<Age;
-    // }
 
+class studentData {
+private:
+    string name="";
+    int age=-999;
+
+public:
+    void setName(const string &inputName) {
+        name = inputName;
+    }
+
+    void setAge(int inputAge) {
+        age = inputAge;
+    }
+
+    string getName() const{
+        return name;
+    }
+
+    int getAge() const {
+        return age;
+    }
+
+    void resetData() {
+        name = "";
+        age = 0;
+    }
 };
-int main () {
-    studentData std;
-    cout<<"Enter 1 to enter info of user"<<'\n';
-    cout<<"Enter 2 to edit user info"<<'\n';
-    cout<<"Enter 3 to delete user info"<<'\n';
-    cout<<"Enter 4 to view user info"<<'\n';
-    cout<<"Press 0 to exit"<<'\n';
-    int option=-99;
-    int answer=-999;
-    cout<<"Enter name of user:";
-    int inputAge=-88;
+
+void enterUserInfo(studentData &std) {
     string inputName;
-    cin >>inputName;
-    cout<<"Enter age of user:";
-    cin>>inputAge;
-    cout<<'\n';
-    while (option!=0){
-        cout<<"Enter your option:";
-        cin>>option;
-        switch(option) {
-            case 1:
-            std.enterInfo(inputName, inputAge);
+    int inputAge;
+    cout << "Enter name of user:";
+    cin >> inputName;
+    cout << "Enter age of user:";
+    cin >> inputAge;
+    std.setName(inputName);
+    std.setAge(inputAge);
+    cout << "DATA HAS BEEN ENTERED!" << '\n';
+}
+
+void editUserInfo(studentData &std) {
+    char choice;
+    string inputName;
+    int inputAge;
+    cout << "Do you want to edit name or age? (N/A):";
+    cin >> choice;
+    cout << '\n';
+    if (choice == 'N' || choice == 'n') {
+        cout << "Enter name again:";
+        cin >> inputName;
+        std.setName(inputName);
+    } else if (choice == 'A' || choice == 'a') {
+        cout << "Enter age again:";
+        cin >> inputAge;
+        std.setAge(inputAge);
+    }
+    cout << "DATA HAS BEEN EDITED" << '\n';
+}
+
+void deleteUserInfo(studentData &std) {
+    std.resetData();
+    cout << "DATA HAS BEEN DELETED!" << '\n';
+}
+
+void viewUserInfo(const studentData &std) {
+    cout << "Name:" << std.getName() << '\n';
+    cout << "Age:" << std.getAge() << '\n';
+}
+
+int main() {
+    studentData std;
+    int option = -99;
+
+    while (option != 0) {
+        cout << "Enter 1 to enter info of user" << '\n';
+        cout << "Enter 2 to edit user info" << '\n';
+        cout << "Enter 3 to delete user info" << '\n';
+        cout << "Enter 4 to view user info" << '\n';
+        cout << "Press 0 to exit" << '\n';
+        cout << "Enter your option:";
+        cin >> option;
+
+        switch (option) {
+        case 1:
+            enterUserInfo(std);
             break;
-            case 2:
-            char choice;
-        char choice2;
-        do {
-            cout<<"Do you want to edit student information? (Y/N):";
-            cin>>choice;
-            cout<<'\n';
-            if (choice=='Y' || choice=='y') {
-                cout<<"Do you want to edit name or age?(N/A):";
-                cin>>choice2;
-                cout<<'\n';
-                if (choice2=='N'|| choice2=='n'){
-                    break;
-                }
-                if (choice2=='A'|| choice=='a'){
-                    cout<<"Enter age again:";
-                    cin>>inputAge;
-                    age=inputAge;
-                    cout<< age;
-                    cout<<'\n';
-                }
-            }
-        }while (choice=='Y' || choice=='y');
-        cout<<"DATA HAS BEEN EDITED"<<'\n';
-            std.editInfo();
+        case 2:
+            editUserInfo(std);
             break;
-            case 3:
-            std.deleteData();
-            // case 4:
+        case 3:
+            deleteUserInfo(std);
+            break;
+        case 4:
+            viewUserInfo(std);
+            break;
         }
     }
 }
